5d9c24902a89e27aa4dc928b2c6d2b375026e966
gototoday <130024882+gototoday@users.noreply.github.com>
2025-01-25 18:56:44 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'd8e02b39-e6a8-439d-a2ef-031a6029393a';
 let proxyIP = '';
 //let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
